00a0623a7f4df34675e1c56901a5483c233b6973
Szybet <53944559+Szybet@users.noreply.github.com>
2025-06-05 11:00:15 +0200

    upp

--- snake_lib/src/consts.rs
+++ snake_lib/src/consts.rs
@@ -1,23 +1,26 @@
 use crate::Size;
 use slint::Color;
 
-pub const GRID_SIZE_SIMPLE: i32 = 10;
-pub const GRID_SIZE: Size = Size {
-    width: GRID_SIZE_SIMPLE,
-    height: GRID_SIZE_SIMPLE,
-};
 pub const BLOCK_BORDER_WIDTH: f32 = 1.0;
 pub const BLOCK_BORDER_COLOR: Color = Color::from_rgb_u8(0, 0, 0);
 pub const BLOCK_COLOR_USED: Color = Color::from_rgb_u8(0, 0, 0);
 
 cfg_if::cfg_if! {
-    if #[cfg(any(
-        target_os = "none"
+    if #[cfg(all(
+        target_os = "none",
+        not(target_arch = "x86_64")
     ))] {
         pub const BLOCK_COLOR_UNUSED: Color = Color::from_rgb_u8(255, 255, 255);
         pub const LOOP_DELAY_MS: u64 = 10;
+        pub const GRID_SIZE_SIMPLE: i32 = 6;
     } else {
         pub const BLOCK_COLOR_UNUSED: Color = Color::from_rgb_u8(0, 153, 51);
         pub const LOOP_DELAY_MS: u64 = 400;
+        pub const GRID_SIZE_SIMPLE: i32 = 10;
     }
 }
+
+pub const GRID_SIZE: Size = Size {
+    width: GRID_SIZE_SIMPLE,
+    height: GRID_SIZE_SIMPLE,
+};
