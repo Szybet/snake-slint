cd61062eee3fbae8d154b0c8fb959061136d61b2
Szybet <53944559+Szybet@users.noreply.github.com>
2025-06-11 23:36:52 +0200

    Update build.rs

--- snake_lib/build.rs
+++ snake_lib/build.rs
@@ -2,7 +2,7 @@ fn main() {
     slint_build::compile_with_config(
         "ui/app-window.slint",
         slint_build::CompilerConfiguration::new()
-            .with_style("fluent-dark".to_owned())
+            .with_style("fluent-light".to_owned())
             .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer)
             .with_sdf_fonts(true),
     )
